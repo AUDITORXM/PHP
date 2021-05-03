d60de4d58227f779f6b142c737b7a8c8bacc06a4
BLANCO CAMPANDEGI KEVIN <44571028H@cebancdea.edu>
2021-05-03 20:48:31 +0200

    Añadido icono y cambiado Copy

--- Proyecto/includes/inc_config.php
+++ Proyecto/includes/inc_config.php
@@ -9,6 +9,7 @@ if (isset($_SESSION['logeado']) && $_SESSION['logeado'] == TRUE) {
 }?>
 
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<link rel="icon" href="https://img.icons8.com/pastel-glyph/344/movie-beginning.png">
 <!-- Bootstrap -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
--- Proyecto/includes/inc_pie.php
+++ Proyecto/includes/inc_pie.php
@@ -1,7 +1,7 @@
 	<footer class="bg-dark text-white text-center text-lg-start">
 	<!-- Copyright -->
 		<div class="text-center p-3">
-			<span class="copyleft">&copy;</span> 2021 Cuevana 4
+			<span class="copyleft">&copy;</span> 2021 Mr. White
 		</div>
 	<!-- Copyright -->
 	</footer>
